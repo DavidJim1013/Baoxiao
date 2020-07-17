f7728507087ec953b6cefa6c3230bcbbbc0aa245
戴伟健 <daiweijian1013@hotmail.com>
2020-07-17 16:07:53 +0800

    style

--- js/index.js
+++ js/index.js
@@ -7,6 +7,14 @@ jsondata = JSON.parse(data);
 url = jsondata["url"]
 
 //判断输入订单号是否为数字，为数字返回true
+function Null() {
+  let a = $("#addnums").val();
+  if (a == '') {
+    $("#status").attr("class", "alert alert-danger")
+    document.getElementById("statusText").innerHTML = "请输入报销单号！"
+  } else search()
+}
+
 function isNum() {
   let a = $("#addnums").val();
   let b = isNaN(a)
@@ -15,10 +23,17 @@ function isNum() {
   } else return true
 }
 
+function moneyform(a) {
+  if (isNaN(a)) {
+    return false
+  } else return true
+}
+
+
 //判断键盘输入是否为回车
 function searchEnter() {
   if (event.keyCode == 13) {
-    search()
+    Null()
   }
 }
 
@@ -38,20 +53,19 @@ function search() {
       success: (res) => {
         if (res == "0") {
           $("#status").attr("class", "alert alert-danger")
-          document.getElementById("statusText").innerHTML="单号不存在！"
-          alert("单号不存在")
+          document.getElementById("statusText").innerHTML = "单号不存在！"
         } else {
           var add = []
           var inputTexts = $(".input-texts")
           status = res[0].restf
-          if (status == "审批通过"){
-            document.getElementById("statusText").innerHTML="审批已通过！"
+          if (status == "审批通过") {
+            document.getElementById("statusText").innerHTML = "审批已通过！"
             $("#status").attr("class", "alert alert-success")
-          } else if (status == "驳回"){
-            document.getElementById("statusText").innerHTML="报销已驳回！"
+          } else if (status == "驳回") {
+            document.getElementById("statusText").innerHTML = "报销已驳回！"
             $("#status").attr("class", "alert alert-danger")
-          } else if (status == "未审核"){
-            document.getElementById("statusText").innerHTML="报销待审核中！"
+          } else if (status == "未审核") {
+            document.getElementById("statusText").innerHTML = "报销待审核中！"
             $("#status").attr("class", "alert alert-warning")
           }
           for (let i = 0; i < res.length; i++) {
@@ -70,9 +84,22 @@ function search() {
   }
   else {
     $("#status").attr("class", "alert alert-danger")
-    alert("单号输入有误")
+    document.getElementById("statusText").innerHTML = "单号输入有误"
   }
 }
+
+$("input[type=file]").change(function () {
+  var fieldVal = $(this).val();
+
+  // Change the node's value by removing the fake path (Chrome)
+  fieldVal = fieldVal.replace("C:\\fakepath\\", "");
+
+  if (fieldVal != undefined || fieldVal != "") {
+    $(this).next(".custom-file-label").attr('data-content', fieldVal);
+    $(this).next(".custom-file-label").text(fieldVal);
+  }
+});
+
 
 //获取inputs数据，存储图片
 function submit() {
@@ -89,13 +116,17 @@ function submit() {
   console.log(formData)
 
   if (texts.name == '') {
-    alert("报销人姓名不能为空")
-  } else if (texts.oodnumber == '') {
-    alert("报销单号不能为空")
-  } else if (texts.money == '') {
-    alert("报销金额不能为空")
+    $("#status2").attr("class", "alert alert-danger")
+    document.getElementById("statusText2").innerHTML = "报销人姓名不能为空"
+  } else if (texts.oodnumber == '' || !moneyform(texts.oodnumber)) {
+    $("#status2").attr("class", "alert alert-danger")
+    document.getElementById("statusText2").innerHTML = "报销单号输入有误"
+  } else if (texts.money == '' || !moneyform(texts.money)) {
+    $("#status2").attr("class", "alert alert-danger")
+    document.getElementById("statusText2").innerHTML = "报销金额输入有误"
   } else if (file == null) {
-    alert("请选择报销凭证图片")
+    $("#status2").attr("class", "alert alert-danger")
+    document.getElementById("statusText2").innerHTML = "请选择报销凭证图片"
   } else {
 
     $.ajax({
@@ -122,12 +153,11 @@ function submit() {
       dataType: "json",
       success: (e) => {
         if (e == "0") {
-          alert("上传成功")
-          location.replace(location)
+          $("#status2").attr("class", "alert alert-success")
+          document.getElementById("statusText2").innerHTML = "上传成功"
         } else if (e == "1") {
-          alert("报销单号已存在")
-        } else if (e == "2") {
-          alert("请输入报销单号")
+          $("#status2").attr("class", "alert alert-warning")
+          document.getElementById("statusText2").innerHTML = "报销单号已存在"
         }
       }
     })
