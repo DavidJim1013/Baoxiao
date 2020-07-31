0ebd4d17db2e2cfb1fb43029a9651e6cb661024e
戴伟健 <daiweijian1013@hotmail.com>
2020-07-31 16:40:45 +0800

    url

--- js/token.js
+++ js/token.js
@@ -1,18 +1,12 @@
 $(() => {
-  var path = "config.json";
-  var request = new XMLHttpRequest();
-  request.open("GET", path, false);
-  request.send();
-  data = request.responseText;
-  jsondata = JSON.parse(data);
-  url = jsondata["url"]
 
+  
   let token = localStorage.getItem("token");
   let data = {
     "token": token
   }
   $.ajax({
-    url: url + "index",
+    url: "https://baoxiao.daiweijian.cn/index",
     type: "post",
     dataType: "json",
     contentType: "application/json",
